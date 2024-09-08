b730a3c349cbf5989533d303de1aa71609e426df
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-09-07 18:30:55 -0700

    creating ship.py
    1. creating Ship class
    2. display ship successfully

--- Space_Invader/main.py
+++ Space_Invader/main.py
@@ -1,12 +1,16 @@
 import pygame
 import sys
 from settings import game_settings
+from ship import Ship
 
 def run_game():
     pygame.init()
 
     screen = pygame.display.set_mode((game_settings.WINDOW_WIDTH,game_settings.WINDOW_HEIGHT))# 创建一个游戏窗口界面
     pygame.display.set_caption("Alien Invasion")# 设置游戏的名称
+
+    # 创建一个飞船的object
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     game_running = True
@@ -18,6 +22,7 @@ def run_game():
 
         # 每次循环时，都重新绘制屏幕颜色
         screen.fill(game_settings.bg_color)
+        ship.display_ship()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
--- Space_Invader/ship.py
+++ Space_Invader/ship.py
@@ -1,3 +1,5 @@
+import pygame
+from settings import game_settings
 
 class Ship:
     """
@@ -5,5 +7,13 @@ class Ship:
 
         创建一个飞船类，用以管理飞船所有的属性和动作。
     """
-    def __init__(self):
-        pass
+    def __init__(self, display_screen):
+        self.screen = display_screen
+
+        # 加载飞船的图像并且设定显示位置
+        self.image = pygame.image.load("./Space_Invader/assets/images/player_ship.png")
+        self.rect = self.image.get_rect() # 外接矩形
+        self.rect.center = (game_settings.WINDOW_WIDTH//2, (game_settings.WINDOW_HEIGHT-35))
+
+    def display_ship(self):
+        self.screen.blit(self.image, self.rect)
