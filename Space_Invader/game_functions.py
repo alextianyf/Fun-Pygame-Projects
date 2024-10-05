1d61f55128270a769c642b0baea8b19b79c6af39
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-10-05 10:42:16 -0700

    update

--- Space_Invader/game_functions.py
+++ Space_Invader/game_functions.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 from bullet import Bullet
+def fire_bullet(bullets_group,screen,game_settings,ship):
+    if len(bullets_group) < game_settings.bullet_num_allowed:
+                bullets_group.add(Bullet(screen, game_settings, ship))
 
 def check_keydown(screen, game_settings,ship, bullets_group, event):
     if event.key == pygame.K_RIGHT:
@@ -8,9 +11,7 @@ def check_keydown(screen, game_settings,ship, bullets_group, event):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        if len(bullets_group) < game_settings.bullet_num_allowed:
-            new_bullet = Bullet(screen, game_settings, ship)
-            bullets_group.add(new_bullet)
+        fire_bullet(bullets_group, screen, game_settings,ship)
 
 def check_keyup(ship, event):
     if event.key == pygame.K_RIGHT:
@@ -28,6 +29,17 @@ def check_mouse_key_events(screen, game_settings,ship, bullets_group):
         elif event.type == pygame.KEYUP:
             check_keyup(ship, event)
 
+def update_bullet(bullets_group):
+    for bullet in bullets_group.sprites():
+        bullet.draw_bullet()
+        bullet.display_bullet()
+    
+    for bullet in bullets_group.sprites():
+        if bullet.rect.bottom <= 0:
+            bullets_group.remove(bullet)
+    # Debugging purpose
+    #print(len(bullets_group))
+
 def update_screen(screen, game_settings, ship, bullets_group):
     # 每次循环时，都重新绘制屏幕颜色
     screen.fill(game_settings.bg_color)
@@ -37,13 +49,7 @@ def update_screen(screen, game_settings, ship, bullets_group):
     ship.display_ship()
 
     # 更新显示bullet
-    for bullet in bullets_group.sprites():
-        bullet.draw_bullet()
-        bullet.display_bullet()
-    
-    for bullet in bullets_group.copy():
-        if bullet.rect.bottom <= 0:
-            bullets_group.remove(bullet)
+    update_bullet(bullets_group)
 
     # 让最近绘制的屏幕可见
     pygame.display.flip()
